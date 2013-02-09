83d60829f5af283ed6b33e8a0da5e913c3df3f62
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-02-09 14:17:24 +0100

    version update (0.1a1)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
   name = 'robotframework-tools',
-  version = '0.1a',
+  version = '0.1a1',
   description = (
     'Tools for Robot Framework and Test Libraries.'
     ),
