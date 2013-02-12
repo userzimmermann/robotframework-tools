d9529861dfeef9aa27775bbc3049640bad41c8b2
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-02-12 12:36:57 +0100

    added session handler meta information support for defining prefix/suffix strings for name variants generation

--- robottools/library/session.py
+++ robottools/library/session.py
@@ -38,32 +38,102 @@ class Meta(object):
     """Generate several variants of the session handler name
     for use in identifiers and message strings,
     based on the `handlerclsname`
-    and/or the attributes of a `defs` class.
+    and/or the attributes of a `defs` class,
+    which can define name (variant) prefixes/suffixes
+    and/or explicit name variants.
     """
+    def gen_prefix(key, default, append = ''):
+      """Check the prefix definition for name variant identified by `key`,
+      setting to `default` if not defined.
+      Always `append` the given extra string.
+      """
+      try:
+        prefix = getattr(defs, (key and key + '_') + 'name_prefix')
+      except AttributeError:
+        prefix = default
+      else:
+        prefix = prefix and str(prefix) or ''
+      if prefix:
+        prefix += append
+      return prefix
+
+    # check all prefix definitions and generate actual prefix strings
+    prefix = {}
+    prefix[''] = gen_prefix('', '', '_')
+    prefix['upper'] = gen_prefix('upper', prefix[''].capitalize())
+    prefix['identifier'] = gen_prefix('identifier', '', '_')
+    prefix['upper_identifier'] = gen_prefix(
+      'upper_identifier', prefix['identifier'].capitalize())
+    prefix['verbose'] = gen_prefix('verbose', '', ' ')
+
+    def gen_suffix(key, default, prepend = ''):
+      """Check the suffix definition for name variant identified by `key`,
+      setting to `default` if not defined.
+      Always `prepend` the given extra string.
+      """
+      try:
+        suffix = getattr(defs, key + '_name_suffix')
+      except AttributeError:
+        suffix = default
+      else:
+        suffix = suffix and str(suffix) or ''
+      if suffix:
+        suffix = prepend + suffix
+      return suffix
+
+    # check all suffix definitions and generate actual suffix strings
+    suffix = {}
+    suffix[''] = gen_suffix('', '', '_')
+    suffix['upper'] = gen_suffix('upper', suffix[''].capitalize())
+    suffix['identifier'] = gen_suffix('identifier', 'session', '_')
+    suffix['upper_identifier'] = gen_suffix(
+      'upper_identifier', suffix['identifier'].capitalize())
+    suffix['verbose'] = gen_suffix('verbose', 'Session', ' ')
+
+    # check explicit base name definition
     name = getattr(defs, 'name', None)
     # non-empty string or None
     name = name and str(name) or None
+    # check all explicit name variant definitions
     variants = {}
-    for prefix in (
+    for variantkey in (
       'upper', 'identifier', 'plural_identifier', 'upper_identifier',
       'verbose', 'plural_verbose'
       ):
-      variant = getattr(defs, prefix + '_name', None)
+      variant = getattr(defs, variantkey + '_name', None)
       # non-empty string or None
-      variant = variant and str(variant) or None
-      variants[prefix] = variant
+      variant = variant and (str(variant) or None) or None
+      variants[variantkey] = variant
 
-    self.name = name or handlerclsname.lower()
+    key = ''
+    self.name = (
+      name
+      or prefix[key] + handlerclsname.lower() + suffix[key])
+    key = 'upper'
     self.upper_name = (
-      variants['upper'] or name and name.capitalize() or handlerclsname)
-    self.identifier_name = variants['identifier'] or self.name + '_session'
+      variants[key]
+      or name and name.capitalize()
+      or prefix[key] + handlerclsname + suffix[key])
+    key = 'identifier'
+    self.identifier_name = (
+      variants[key]
+      or prefix[key] + self.name + suffix[key])
+    key = 'plural_identifier'
     self.plural_identifier_name = (
-      variants['plural_identifier'] or self.identifier_name + 's')
+      variants[key]
+      or self.identifier_name + 's')
+    key = 'upper_identifier'
     self.upper_identifier_name = (
-      variants['upper_identifier'] or self.upper_name + 'Session')
-    self.verbose_name = variants['verbose'] or self.upper_name + ' Session'
+      variants[key]
+      or prefix[key] + self.upper_name + suffix[key])
+    key = 'verbose'
+    self.verbose_name = (
+      variants[key]
+      or prefix[key] + self.upper_name + suffix[key])
+    key = 'plural_verbose'
     self.plural_verbose_name = (
-      variants['plural_verbose'] or self.verbose_name + 's')
+      variants[key]
+      or self.verbose_name + 's')
 
 class HandlerMeta(type):
   """The custom type class for :class:`Handler`.
