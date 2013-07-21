0496103cb86ee898b843d973aa8dc627ba8a81b7
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-07-21 22:45:42 +0200

    robottools.library.inspector: ROBOT_LIBRARIES - List with names of installed default robot.libraries - TestLibraryInspectorMeta with __getattr__ access to default TestLibraryInspectors - General code indent cleanup

--- robottools/library/inspector.py
+++ robottools/library/inspector.py
@@ -22,6 +22,7 @@
 .. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
 """
 __all__ = (
+  'ROBOT_LIBRARIES',
   'TestLibraryImportError',
   'TestLibraryInspector', 'MultiTestLibraryInspector',
   )
@@ -30,74 +31,96 @@ from collections import OrderedDict
 from itertools import chain
 from moretools import camelize, decamelize, simpledict
 
+from path import path as Path
+
 import robot.errors
 import robot.running
+import robot.libraries
+
+ROBOT_LIBRARIES_PATH = Path(robot.libraries.__file__).dirname()
+ROBOT_LIBRARIES = set()
+for path in ROBOT_LIBRARIES_PATH.walkfiles():
+    name = str(path.basename().splitext()[0])
+    if name[0].isupper():
+        ROBOT_LIBRARIES.add(name)
+del path, name
+
+ROBOT_LIBRARIES = sorted(ROBOT_LIBRARIES)
 
 class TestLibraryImportError(ImportError):
   pass
 
+class TestLibraryInspectorMeta(type):
+    def __getattr__(self, libname):
+        return TestLibraryInspector(libname)
+
+    def __dir__(self):
+        return list(ROBOT_LIBRARIES)
+
 class TestLibraryInspector(object):
-  def __init__(self, name):
-    try:
-      self._library = robot.running.TestLibrary(name)
-    except robot.errors.DataError as e:
-      raise TestLibraryImportError(str(e))
+    __metaclass__ = TestLibraryInspectorMeta
 
-  @property
-  def name(self):
-    return self._library.name
+    def __init__(self, name):
+        try:
+            self._library = robot.running.TestLibrary(name)
+        except robot.errors.DataError as e:
+            raise TestLibraryImportError(str(e))
 
-  def __iter__(self):
-    for keyword in self._library.handlers.values():
-      yield keyword
+    @property
+    def name(self):
+        return self._library.name
 
-  def __getitem__(self, name):
-    try:
-      return self._library.get_handler(name)
-    except robot.errors.DataError as e:
-      raise KeyError(str(e))
+    def __iter__(self):
+        for keyword in self._library.handlers.values():
+            yield keyword
 
-  def __getattr__(self, name):
-    try:
-      return self[name]
-    except KeyError as e:
-      raise AttributeError(str(e))
+    def __getitem__(self, name):
+        try:
+            return self._library.get_handler(name)
+        except robot.errors.DataError as e:
+            raise KeyError(str(e))
 
-  def __dir__(self):
-    return list(map(camelize, self._library.handlers.keys()))
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError as e:
+            raise AttributeError(str(e))
 
-TestLibrariesDict = simpledict('TestLibrariesDict', dicttype = OrderedDict)
+    def __dir__(self):
+        return list(map(camelize, self._library.handlers.keys()))
+
+TestLibrariesDict = simpledict('TestLibrariesDict', dicttype=OrderedDict)
 
 class MultiTestLibraryInspector(object):
-  def __init__(self, *libraries):
-    libslist = []
-    for lib in libraries:
-      if type(lib) is not TestLibraryInspector:
-        lib = TestLibraryInspector(str(lib))
-      libslist.append(lib)
-    self.libraries = TestLibrariesDict.frozen(
-      (lib.name, lib) for lib in libslist)
+    def __init__(self, *libraries):
+        libslist = []
+        for lib in libraries:
+            if type(lib) is not TestLibraryInspector:
+                lib = TestLibraryInspector(str(lib))
+            libslist.append(lib)
+        self.libraries = TestLibrariesDict.frozen(
+          (lib.name, lib) for lib in libslist)
 
-  def __iter__(self):
-    for libname, lib in self.libraries:
-      for keyword in lib._library.handlers.values():
-        yield keyword
+    def __iter__(self):
+        for libname, lib in self.libraries:
+            for keyword in lib._library.handlers.values():
+                yield keyword
 
-  def __getitem__(self, name):
-    for libname, lib in self.libraries:
-      try:
-        return lib[name]
-      except KeyError as e:
-        pass
-    raise e
+    def __getitem__(self, name):
+        for libname, lib in self.libraries:
+            try:
+                return lib[name]
+            except KeyError as e:
+                pass
+        raise e
 
-  def __getattr__(self, name):
-    try:
-      return self[name]
-    except KeyError as e:
-      raise AttributeError(str(e))
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError as e:
+            raise AttributeError(str(e))
 
-  def __dir__(self):
-    ikeys = chain(*(
-      lib._library.handlers.keys() for libname, lib in self.libraries))
-    return list(map(camelize, ikeys))
+    def __dir__(self):
+        ikeys = chain(*(
+          lib._library.handlers.keys() for libname, lib in self.libraries))
+        return list(map(camelize, ikeys))
