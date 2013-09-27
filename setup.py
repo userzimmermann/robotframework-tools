6e6bbb473b407535c2d2f5b600ef673f80f233a4
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-09-27 15:04:39 +0200

    0.1a75

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
   name = 'robotframework-tools',
-  version = '0.1a74',
+  version = '0.1a75',
   description = (
     'Tools for Robot Framework and Test Libraries.'
     ),
