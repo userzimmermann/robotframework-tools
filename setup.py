d55999b45fc4e4d2f1e02a4c60313ac935fd36fc
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-10-16 10:42:50 +0200

    0.1a89

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info[0] == 3:
 
 setup(
   name = 'robotframework-tools',
-  version = '0.1a88',
+  version = '0.1a89',
   description = (
     'Python Tools for Robot Framework and Test Libraries.'
     ),
