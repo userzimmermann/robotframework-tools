7bbb4c9867dceb705f609cb2de4d2d963d514c3c
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-10-23 13:02:08 +0200

    robotshell.magic: Some module doc string additions

--- robotshell/magic/variable.py
+++ robotshell/magic/variable.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with robotframework-tools. If not, see <http://www.gnu.org/licenses/>.
 
-"""robotshell.magic
+"""robotshell.magic.variable
 
 .. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
 """
