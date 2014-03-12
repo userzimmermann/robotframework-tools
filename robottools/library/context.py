b21b6338c43b47aac68295ebabd3cfce2c8b3778
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-03-12 08:53:43 +0000

    library.context: Fixed module doc.

--- robottools/library/context.py
+++ robottools/library/context.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with robotframework-tools. If not, see <http://www.gnu.org/licenses/>.
 
-"""robottools.session
+"""robottools.context
 
 .. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
 """
