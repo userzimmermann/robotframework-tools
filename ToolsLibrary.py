78e455bf6ab9f3890bd7d0dbfb314cdfbf84499d
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-04-20 22:07:27 +0200

    ToolsLibrary: File header.

--- ToolsLibrary.py
+++ ToolsLibrary.py
@@ -1,3 +1,27 @@
+# robotframework-tools
+#
+# Tools for Robot Framework and Test Libraries.
+#
+# Copyright (C) 2013-2014 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# robotframework-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# robotframework-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with robotframework-tools. If not, see <http://www.gnu.org/licenses/>.
+
+"""ToolsLibrary
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
+
 import robot.running
 from robot.running.namespace import IMPORTER
 
