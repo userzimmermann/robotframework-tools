10881296a33622f910bc8b9671eb06fbe46d96a2
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-06 07:42:37 +0000

    robottools.__init__ header.

--- robottools/__init__.py
+++ robottools/__init__.py
@@ -1,3 +1,27 @@
+# robotframework-tools
+#
+# Python Tools for Robot Framework and Test Libraries.
+#
+# Copyright (C) 2013-2014 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# robotframework-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# robotframework-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with robotframework-tools. If not, see <http://www.gnu.org/licenses/>.
+
+"""robottools
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
+
 from robottools.library import *
 from robottools.library.keywords import *
 from robottools.library.session import Handler as SessionHandler
