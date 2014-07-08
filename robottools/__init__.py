3f0faf4998b3e3a02e2d7986103fd68a8e7b3239
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-08 14:15:02 +0000

    robottools: __version__, __requires__, __extras__

--- robottools/__init__.py
+++ robottools/__init__.py
@@ -24,6 +24,8 @@ Python Tools for Robot Framework and Test Libraries.
 .. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
 """
 
+from robottools import setup
+
 from robottools.library import *
 from robottools.library.keywords import *
 from robottools.library.session import SessionHandler
@@ -32,3 +34,10 @@ from robottools.library.context import *
 from robottools.library.inspector import *
 
 from robottools.testrobot import *
+
+
+__version__ = setup.VERSION
+
+__requires__ = setup.REQUIRES
+
+__extras__ = setup.EXTRAS
