01233e69a1109e3835cd9bb98a87c974e3f78b43
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-12 19:20:03 +0000

    zetup update.

--- zetup.py
+++ zetup.py
@@ -25,7 +25,9 @@ if sys.version_info[0] == 3:
 import os
 import re
 from collections import OrderedDict
-from pkg_resources import parse_version, parse_requirements
+from pkg_resources import (
+  parse_version, parse_requirements,
+  DistributionNotFound, VersionConflict)
 
 try:
     from setuptools import setup
@@ -78,18 +80,59 @@ class Requirements(str):
         return str.__new__(cls, '\n'.join(map(str, reqs)))
 
     def __init__(self, text):
-        self._list = list(parse_requirements(text))
+        """Parse a requirements `text` and store a list
+           of pkg_reqsources.Requirement instances.
+
+        - Additionally looks for "# modname" comments after requirement lines
+          (the actual root module name of the required package)
+          and stores them as .modname attrs on the Requirement instances.
+        """
+        def reqs(): # Generate the parsed requirements from text:
+            for line in text.split('\n'):
+                line = line.strip()
+                if not line:
+                    continue
+                try:
+                    req, modname = line.split('#')
+                except ValueError:
+                    req = next(parse_requirements(line))
+                    req.modname = req.key
+                else:
+                    req = next(parse_requirements(req))
+                    req.modname = modname.strip()
+                yield req
+        self._list = list(reqs())
+
+    def check(self, raise_=True):
+        for req in self:
+            try:
+                mod = __import__(req.modname)
+            except:
+                if raise_:
+                    raise DistributionNotFound(str(req))
+                return False
+            if mod.__version__ not in req:
+                if raise_:
+                    raise VersionConflict(
+                      "Need %s. Found %s" % (str(req), mod.__version__))
+                return False
+        return True
 
     def __iter__(self):
         return iter(self._list)
 
     def __getitem__(self, name):
+        """Get a requirement by its package name.
+        """
         for req in self._list:
             if name in [req.key, req.unsafe_name]:
                 return req
         raise KeyError(name)
 
     def __add__(self, text):
+        """Return a new manager instance
+           with additional requirements from `text`.
+        """
         return type(self)('%s\n%s' % (self, text))
 
     def __repr__(self):
@@ -99,6 +142,10 @@ class Requirements(str):
 class Extras(OrderedDict):
     """Package extra features/requirements manager.
     """
+    def __setitem__(self, name, text):
+        reqs = Requirements(text)
+        OrderedDict.__setitem__(self, name, reqs)
+
     def __repr__(self):
         return "\n\n".join((
           "[%s]\n" % key + '\n'.join(map(str, reqs))
@@ -119,7 +166,7 @@ for fname in sorted(os.listdir('.')):
     if match:
         SETUP_DATA.append(fname)
 
-        EXTRAS[match.group('name')] = Requirements(open(fname).read())
+        EXTRAS[match.group('name')] = open(fname).read()
 
 
 # If installed with pip, add all build directories and src/ subdirs
@@ -172,7 +219,8 @@ else:
 
         def REQUIRES(self, reqs):
             return '\n'.join(
-              "* [`%s`](https://pypi.python.org/pypi/%s)" % (
+              "* [`%s`](\n"
+              "    https://pypi.python.org/pypi/%s)" % (
                 req, req.unsafe_name)
               for req in reqs)
 
@@ -190,17 +238,18 @@ else:
               "\n\n"
               "    pip install ."
               "\n\n"
-              "Or from [PyPI](https://pypi.python.org/pypi/%s):"
+              "Or get the latest release from [PyPI](\n"
+              "  https://pypi.python.org/pypi/%s):"
               "\n\n"
               "    pip install %s"
-              % tuple(2 * [PROJECT]))
+              % tuple(2 * [NAME]))
             if EXTRAS:
               mdtext += (
                 "\n\n"
                 "* With all extra features:"
                 "\n\n"
                 "        pip install %s[%s]"
-                % (PROJECT, ','.join(EXTRAS)))
+                % (NAME, ','.join(EXTRAS)))
             return mdtext
 
 
