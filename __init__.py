04e39ea0376de6dbbd75167147d5f788751ed40b
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-12 20:18:38 +0000

    __init__: Add # robot modname hint to additional robotframework requirement.

--- __init__.py
+++ __init__.py
@@ -41,4 +41,4 @@ except (ImportError, NameError): # No path or __file__
 REQUIRES += (
   'robotframework >= 2.8' if sys.version_info[0] == 2
   else 'robotframework-python3 >= 2.8.4'
-  )
+  ) + ' # robot'
