f46fbf853df494b4c7ba067dd632f4a0a2b37db5
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-15 07:31:21 +0000

    zetup.py-->__init__.py
    --HG--
    rename : zetup.py => __init__.py

--- SConstruct
+++ SConstruct
@@ -1,5 +1,7 @@
 exec(open('__init__.py').read())
 
+REQUIRES += 'robotframework >= 2.8 # robot'
+
 
 env = Environment(BUILDERS={'README': README_Builder()})
 
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import sys
+
+
 exec(open('__init__.py').read())
+
+REQUIRES += (
+  'robotframework >= 2.8' if sys.version_info[0] == 2
+  else 'robotframework-python3 >= 2.8.4'
+  ) + ' # robot'
 
 
 zetup(
