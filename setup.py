70fe2c333b140d26c0ffcdd6109dd6543d25f690
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-11 17:40:06 +0000

    setup: No explicit package_dir, packages, package_data.

--- setup.py
+++ setup.py
@@ -9,27 +9,4 @@ REQUIRES += (
   ) + ' # robot'
 
 
-zetup(
-  package_dir={
-    'robottools.zetup': '.',
-    },
-  packages=[
-    'robottools',
-    'robottools.zetup',
-    'robottools.library',
-    'robottools.library.keywords',
-    'robottools.library.session',
-    'robottools.library.context',
-    'robottools.library.inspector',
-    'robottools.testrobot',
-    'robottools.remote',
-    'robotshell',
-    'robotshell.magic',
-    ],
-  py_modules=[
-    'ToolsLibrary',
-    ],
-  package_data={
-    'robottools.zetup': ZETUP_DATA,
-    },
-  )
+zetup(py_modules=['ToolsLibrary'])
