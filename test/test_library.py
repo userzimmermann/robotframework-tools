f88f4ea16ad604ab61b76c5b6c0e9123b3bb01f3
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-01-02 17:21:57 +0000

    test_library

---
+++ test/test_library.py
@@ -0,0 +1,37 @@
+import pytest
+
+from robottools import testlibrary, istestlibraryclass
+
+
+def test_testlibrary():
+    """Test creation of Dynamic Test Library classes.
+    """
+    TestLibrary = testlibrary()
+
+    assert istestlibraryclass(TestLibrary)
+
+    assert not TestLibrary.session_handlers
+    assert not TestLibrary.context_handlers
+    assert not TestLibrary.keywords
+
+    @TestLibrary.keyword
+    def test_keyword(self):
+        pass
+
+    class LibraryNotCallingBaseInit(TestLibrary):
+        def __init__(self):
+            pass
+
+    assert istestlibraryclass(LibraryNotCallingBaseInit)
+
+    lib = LibraryNotCallingBaseInit()
+
+    # check if methods complain about missing instance-bound keywords mapping
+    for method, args in [
+      (lib.get_keyword_names, []),
+      (lib.get_keyword_arguments, ['test_keyword']),
+      (lib.get_keyword_documentation, ['test_keyword']),
+      (lib.run_keyword, ['test_keyword']),
+      ]:
+        with pytest.raises(RuntimeError):
+            method(*args)
